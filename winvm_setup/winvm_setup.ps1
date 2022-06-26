0231aa95f3c7a1a15d7de438b9d193d0f3366f2e
Yuki Hanayama <yukiuuh@gmail.com>
2022-06-26 14:24:35 +0900

    first

---
+++ winvm_setup/winvm_setup.ps1
@@ -0,0 +1,65 @@
+write-output "Running User Data Script"
+write-host "(host) Running User Data Script"
+
+Set-ExecutionPolicy Unrestricted -Scope LocalMachine -Force -ErrorAction Ignore
+
+# Don't set this before Set-ExecutionPolicy as it throws an error
+$ErrorActionPreference = "stop"
+
+# Remove HTTP listener
+Remove-Item -Path WSMan:\Localhost\listener\listener* -Recurse
+
+# WinRM
+write-output "Setting up WinRM"
+write-host "(host) setting up WinRM"
+
+cmd.exe /c winrm quickconfig -q
+cmd.exe /c winrm quickconfig '-transport:http'
+cmd.exe /c winrm set "winrm/config" '@{MaxTimeoutms="1800000"}'
+cmd.exe /c winrm set "winrm/config/winrs" '@{MaxMemoryPerShellMB="1024"}'
+cmd.exe /c winrm set "winrm/config/service" '@{AllowUnencrypted="true"}'
+cmd.exe /c winrm set "winrm/config/client" '@{AllowUnencrypted="true"}'
+cmd.exe /c winrm set "winrm/config/service/auth" '@{Basic="true"}'
+cmd.exe /c winrm set "winrm/config/client/auth" '@{Basic="true"}'
+cmd.exe /c winrm set "winrm/config/service/auth" '@{CredSSP="true"}'
+cmd.exe /c winrm set "winrm/config/listener?Address=*+Transport=HTTP" '@{Port="5985"}'
+cmd.exe /c netsh advfirewall firewall set rule group="remote administration" new enable=yes
+cmd.exe /c netsh advfirewall firewall add rule name="Open Port 5985" dir=in action=allow protocol=TCP localport=5985
+cmd.exe /c net stop winrm
+cmd.exe /c sc config winrm start= auto
+cmd.exe /c net start winrm 
+
+# Disable UAC
+write-output "Disabling UAC"
+write-host "(host) Disabling UAC"
+
+Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\Software\Microsoft\Windows\CurrentVersion\Policies\System -Name ConsentPromptBehaviorAdmin -Value 0
+
+# Disable Firewall
+write-output "Disabling Windows Firewall"
+write-host "(host) Disabling WindowsFirewall"
+
+Get-NetFirewallProfile | Set-NetFirewallProfile -Enabled false
+
+# Disable IE Enhanced Security Configuration
+write-output "Disabling IE ESC"
+write-host "(host) Disabling IE ESC"
+Set-ItemProperty -Path "HKLM:¥SOFTWARE¥Microsoft¥Active Setup¥Installed Components¥{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}" -Name "IsInstalled" -Value 0 -Force;
+Set-ItemProperty -Path "HKLM:¥SOFTWARE¥Microsoft¥Active Setup¥Installed Components¥{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}" -Name "IsInstalled" -Value 0 -Force;
+Stop-Process -Name Explorer -Force;
+
+# Enable RDP
+write-output "Enabling RDP"
+write-host "(host) Enabling RDP"
+Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name "fDenyTSConnections" -Value "0"
+
+
+# Disable Server Manager at login
+write-output "Disabling Server Manager at login"
+write-host "(host) Disabling Server Manager at login"
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\ServerManager' -Name "DoNotOpenServerManagerAtLogon" -Value 1
+
+# Install Chocolatey
+write-output "Installing Chocolatey"
+write-host "(host) Installing Chocolatey"
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
